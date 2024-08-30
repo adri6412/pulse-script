d032805058cc56df3acc8a93c11db74d8e935cb5
adri6412 <32240572+adri6412@users.noreply.github.com>
2024-08-30 16:31:56 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #rpm --import https://swdist.triumf.ca/repos/rpm/RPM-GPG-KEY-triumf-swdist                                                                                                                                                 
 #dnf install https://swdist.triumf.ca/repos/rpm/triumf-swdist-repo.fedora.rpm -y
-dnf install systemd dbus-common -y
-dnf install python3-tkinter openconnect vpnc vpnc-script python3-pip
+#dnf install systemd dbus-common -y
 #dnf install pulsesecure -y
-dnf -y install openconnect NetworkManager-openconnect NetworkManager-openconnect-gnome
+#dnf -y install openconnect NetworkManager-openconnect NetworkManager-openconnect-gnome
 dnf install python3-tkinter openconnect vpnc vpnc-script python3-pip
 wget- O /opt/oc-gui.py https://github.com/satyarsh/OpenConnect-Linux-GUI/raw/main/oc-gui.py
