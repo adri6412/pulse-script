82dddff245e1b8fb2814022f1510d3d56b39cfa4
adri6412 <32240572+adri6412@users.noreply.github.com>
2024-08-30 15:57:38 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rpm --import https://swdist.triumf.ca/repos/rpm/RPM-GPG-KEY-triumf-swdist                                                                                                                                                 
 dnf install https://swdist.triumf.ca/repos/rpm/triumf-swdist-repo.fedora.rpm -y
+dnf install systemd dbus-common
 dnf install pulsesecure -y
