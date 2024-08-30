f371a51a1e50c2c336869398dc255edc38291843
adri6412 <32240572+adri6412@users.noreply.github.com>
2024-08-30 16:07:46 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rpm --import https://swdist.triumf.ca/repos/rpm/RPM-GPG-KEY-triumf-swdist                                                                                                                                                 
-dnf install https://swdist.triumf.ca/repos/rpm/triumf-swdist-repo.fedora.rpm -y
+#rpm --import https://swdist.triumf.ca/repos/rpm/RPM-GPG-KEY-triumf-swdist                                                                                                                                                 
+#dnf install https://swdist.triumf.ca/repos/rpm/triumf-swdist-repo.fedora.rpm -y
 dnf install systemd dbus-common -y
-dnf install pulsesecure -y
-dnf -y install openconnect NetworkManager-openconnect NetworkManager-openconnect-gnome
+#dnf install pulsesecure -y
+#dnf -y install openconnect NetworkManager-openconnect NetworkManager-openconnect-gnome
